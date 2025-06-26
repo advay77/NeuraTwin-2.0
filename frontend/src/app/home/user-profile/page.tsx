7d0d49d7a4ff911a354ec976e6ec62a28c5a510d
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2025-06-26 14:09:54 +0530

    fixes...

--- frontend/src/app/home/user-profile/page.tsx
+++ frontend/src/app/home/user-profile/page.tsx
@@ -1,7 +1,19 @@
+"use client";
 import React from "react";
+import { useAppContext } from "@/context/AppContext";
+import UserProfile from "@/components/userProfileData";
 
-const page = () => {
-  return <div></div>;
+const Page = () => {
+  const { currentUser } = useAppContext();
+
+  if (!currentUser)
+    return (
+      <p className="w-full h-full flex items-center justify-center text-white text-lg font-orbitron">
+        Loading...
+      </p>
+    );
+
+  return <UserProfile currentUser={currentUser} />;
 };
 
-export default page;
+export default Page;
